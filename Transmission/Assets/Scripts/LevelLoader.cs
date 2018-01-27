a4a01d574bf826f13268af9bd8f85de5802a39bc
churchmf <churchmf@gmail.com>
2018-01-26 20:27:56 -0700

    level loader static class

---
+++ Transmission/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public static class LevelLoader
+{
+    public static MusicSheet Load(string fileName)
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+        if(File.Exists(filePath)) {
+            return JsonUtility.FromJson<MusicSheet>(File.ReadAllText(filePath));
+        }
+
+        return null;
+    }
+}
+
+[Serializable]
+public class MusicSheet
+{
+    public Note[] notes;
+}
+
+[Serializable]
+public class Note
+{
+    public string note;
+    public float time;
+    public float duration;
+}
