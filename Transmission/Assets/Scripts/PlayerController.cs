82fad8731b9d90e7c05eaced0766607c3ea2f10a
churchmf <churchmf@gmail.com>
2018-01-26 19:39:21 -0700

    update sprite and controller name

---
+++ Transmission/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+}
