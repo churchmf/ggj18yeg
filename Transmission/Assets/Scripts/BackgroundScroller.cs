344f81d1575dce53c0af5cc0403be319987ffd3f
graves <graves@ualberta.ca>
2018-01-28 15:16:28 -0700

    Add background scrolling.

--- Transmission/Assets/Scripts/BackgroundScroller.cs
+++ Transmission/Assets/Scripts/BackgroundScroller.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class BackgroundScroller : MonoBehaviour
 {
 	public float scrollSpeed;
-	public float tileSizeZ;
 
 	private Vector3 startPosition;
+	private float spriteWidth;
 
 	void Start ()
 	{
 		startPosition = transform.position;
+		SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		spriteWidth = spriteRenderer.bounds.size.x;
 	}
 
 	void Update ()
 	{
-		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
-		transform.position = startPosition + Vector3.forward * newPosition;
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, spriteWidth*1.5f);
+		transform.position = startPosition + Vector3.left * newPosition;
 	}
 }
